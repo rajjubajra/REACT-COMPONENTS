8fb9084adb1c1ee1ef51eb25c732312ebeba1d4d
rajjubajra <rajjubajra@gmail.com>
2021-09-26 12:55:21 +0100

    updates tour

--- react-tailwind-components/src/components/Music/TourList02.js
+++ react-tailwind-components/src/components/Music/TourList02.js
@@ -6,28 +6,23 @@ function TourList02({date, title, venue, view}) {
 
 
   return (
-      <div>
-
-        <h2 className="text-3xl">Tour</h2>
-        <div className="grid grid-cols-12 grid-flow-row gap-2 py-5
+      <div className="grid grid-cols-12 grid-flow-row gap-2 py-5
               border-t border-gray-300">
-                <div className="col-span-12">{date}</div>
-                <div className="col-span-12">
+          <div className="col-span-12">{date}</div>
+              <div className="col-span-12">
                   <h2 className="text-2xl">{title}</h2>
                   <div>{venue}</div>
-                </div>
-                <div className="col-span-6 border border-gray-200">
+              </div>
+              <div className="col-span-6 border border-gray-200">
                   <div className="cursor-pointer p-2 text-sm flex"
                     onClick={() => view} >
                   <div>View Detail</div>
                   <div className="pt-1 px-2"><GrFormNextLink /></div>
-                </div>
-                </div>
-                <div className="col-span-6 border border-gray-200 p-2">Ticket</div>
               </div>
+          </div>
+          <div className="col-span-6 border border-gray-200 p-2">Ticket</div>
       </div>
-
-  )
+    )
 }
 
 export default TourList02
